7f71272367718c5bf27676051a96a0d3b842271a
Punnathat.K <88327066+sogood-developer@users.noreply.github.com>
2025-07-06 22:32:12 +0700

    Deploy static site to gh-pages

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/sogood/Documents/PROJECT/FASTWORK/Docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/sogood/Documents/PROJECT/FASTWORK/Docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/sogood/Documents/PROJECT/FASTWORK/Docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/sogood/Documents/PROJECT/FASTWORK/Docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/sogood/Documents/PROJECT/FASTWORK/Docs/src/css/custom.css"),
-];
